142d1936d9d12746d7c7cac926993ca73da00675
Estelle Gresillon <estelle.gresillon@kardinal.ai>
2022-03-05 17:34:32 +0100

    fixed menu css

--- src/components/common/ProdMenu/index.jsx
+++ src/components/common/ProdMenu/index.jsx
@@ -39,21 +39,39 @@ const ProdMenuWrapper = styled.div`
   .project-logo {
     font-size: 20px;
     font-weight: bolder;
+    white-space: nowrap;
   }
 
   .project-menu {
-    font-size: 16px;
     display: flex;
+    font-size: 16px;
+    white-space: nowrap;
   }
 
   @media screen and (max-width: 768px) {
-    padding: 0;
-    width: 100%;
+    padding: 0 10px;
+    width: calc(100% - 20px);
+
+    .project-logo {
+      font-size: 16px;
+    }
+
+    .project-menu {
+      font-size: 12px;
+    }
   }
 
   body#force-mobile & {
-    padding: 0;
-    width: 100%;
+    padding: 0 10px;
+    width: calc(100% - 20px);
+
+    .project-logo {
+      font-size: 16px;
+    }
+
+    .project-menu {
+      font-size: 12px;
+    }
   }
 `;
 
@@ -62,4 +80,12 @@ const Item = styled.div`
   margin: 20px;
   text-align: center;
   text-decoration: none;
+
+  @media screen and (max-width: 768px) {
+    margin: 10px;
+  }
+
+  body#force-mobile & {
+    margin: 10px;
+  }
 `;
